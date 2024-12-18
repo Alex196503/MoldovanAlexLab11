25da6af0f7f7256b135b323c55b79e9612069eb3
Alexandru Moldovan <alexmoldovan2009@gmail.com>
2024-12-18 20:13:20 +0200

    Add project files.

---
+++ Data/Moldovan_Alex_Lab11Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Alex_Lab11.Models;
+
+namespace Moldovan_Alex_Lab11.Data
+{
+    public class Moldovan_Alex_Lab11Context : DbContext
+    {
+        public Moldovan_Alex_Lab11Context (DbContextOptions<Moldovan_Alex_Lab11Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moldovan_Alex_Lab11.Models.ShopList> ShopList { get; set; } = default!;
+    }
+}
